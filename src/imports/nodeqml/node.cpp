f9365d3ac09cddd96800abfad5608352c8195a03
Oleg Shparber <trollixx@gmail.com>
2014-09-30 12:12:33 -0700

    Add set/clearTimeout() and set/clearInterval() to Node module

--- src/imports/nodeqml/node.cpp
+++ src/imports/nodeqml/node.cpp
@@ -6,6 +6,12 @@
 #include "modules/util.h"
 
 #include <QJSEngine>
+#include <QLoggingCategory>
+#include <QTimerEvent>
+
+namespace {
+const QLoggingCategory logCategory("nodeqml.core");
+}
 
 Node::Node(QQmlEngine *qmlEngine, QJSEngine *jsEngine, QObject *parent) :
     QObject(parent),
@@ -21,6 +27,82 @@ QJSValue Node::require(const QString &module)
         return m_jsEngine->newQObject(m_coreModules.value(module));
     else
         return m_jsEngine->newObject(); /// TODO: Implement real requre()
+}
+
+QJSValue Node::setTimeout(QJSValue cb, int ms)
+{
+    if (!cb.isCallable()) {
+        qCWarning(logCategory, "Invalid callback.");
+        return QJSValue::NullValue;
+    }
+
+    if (ms <= 0)
+        ms = 1;
+
+    int timerId = startTimer(ms, Qt::PreciseTimer);
+    if (!timerId) {
+        qCWarning(logCategory, "Cannot set timer.");
+        return QJSValue::NullValue;
+    }
+    m_timeoutCallbacks.insert(timerId, cb);
+    /// TODO: Return an object similar to Node's
+    return timerId;
+}
+
+void Node::clearTimeout(QJSValue t)
+{
+    int timerId = t.toInt();
+    if (!m_timeoutCallbacks.contains(timerId))
+        return;
+    killTimer(timerId);
+    m_timeoutCallbacks.remove(timerId);
+}
+
+QJSValue Node::setInterval(QJSValue cb, int ms)
+{
+    if (!cb.isCallable()) {
+        qCWarning(logCategory, "Invalid callback.");
+        return QJSValue::NullValue;
+    }
+
+    if (ms <= 0)
+        ms = 1;
+
+    int timerId = startTimer(ms, Qt::PreciseTimer);
+    if (!timerId) {
+        qCWarning(logCategory, "Cannot set timer.");
+        return QJSValue::NullValue;
+    }
+    m_intervalCallbacks.insert(timerId, cb);
+    /// TODO: Return an object similar to Node's
+    return timerId;
+}
+
+void Node::clearInterval(QJSValue t)
+{
+    int timerId = t.toInt();
+    if (!m_intervalCallbacks.contains(timerId))
+        return;
+    killTimer(timerId);
+    m_intervalCallbacks.remove(timerId);
+}
+
+void Node::timerEvent(QTimerEvent *event)
+{
+    int timerId = event->timerId();
+    QJSValue cb;
+
+    if (m_timeoutCallbacks.contains(timerId)) {
+        killTimer(timerId);
+        cb = m_timeoutCallbacks.take(timerId);
+    } else if (m_intervalCallbacks.contains(timerId)) {
+        cb = m_intervalCallbacks.value(timerId);
+    } else {
+        return;
+    }
+
+    event->accept();
+    cb.call();
 }
 
 void Node::setupCoreModules()
--- src/imports/nodeqml/node.h
+++ src/imports/nodeqml/node.h
@@ -17,9 +17,17 @@ public:
     explicit Node(QQmlEngine *qmlEngine, QJSEngine *jsEngine, QObject *parent = 0);
 
     Q_INVOKABLE QJSValue require(const QString &module);
-signals:
 
-public slots:
+    /// Timers module
+    /// TODO: Move to somewhere else and export globally
+    Q_INVOKABLE QJSValue setTimeout(QJSValue cb, int ms);
+    Q_INVOKABLE void clearTimeout(QJSValue t);
+
+    Q_INVOKABLE QJSValue setInterval(QJSValue cb, int ms);
+    Q_INVOKABLE void clearInterval(QJSValue t);
+
+protected:
+    void timerEvent(QTimerEvent *event);
 
 private:
     void setupCoreModules();
@@ -27,6 +35,8 @@ private:
     QJSEngine *m_jsEngine;
     QQmlEngine *m_qmlEngine;
     QHash<QString, CoreModule *> m_coreModules;
+    QHash<int, QJSValue> m_timeoutCallbacks;
+    QHash<int, QJSValue> m_intervalCallbacks;
 };
 
 #endif // NODE_H
