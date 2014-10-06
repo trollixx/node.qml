70f18820d4ae518e7952cb857732f1aadf1066d4
Oleg Shparber <trollixx@gmail.com>
2014-10-05 23:31:09 -0700

    Add bunch of TODOs in Process module

--- src/imports/nodeqml/modules/process.h
+++ src/imports/nodeqml/modules/process.h
@@ -9,6 +9,43 @@ class Process : public CoreModule
 public:
     explicit Process(QJSEngine *jsEngine, QObject *parent = nullptr);
 
+    /// TODO: Event: 'exit'
+    /// TODO: Event: 'uncaughtException'
+    /// TODO: Signal Events
+    /// TODO: process.stdout
+    /// TODO: process.stderr
+    /// TODO: process.stdin
+    /// TODO: process.argv
+    /// TODO: process.execPath
+    /// TODO: process.execArgv
+    /// TODO: process.abort()
+    /// TODO: process.chdir(directory)
+    /// TODO: process.cwd()
+    /// TODO: process.env
+    /// TODO: process.exit([code])
+    /// TODO: process.getgid()
+    /// TODO: process.setgid(id)
+    /// TODO: process.getuid()
+    /// TODO: process.setuid(id)
+    /// TODO: process.getgroups()
+    /// TODO: process.setgroups(groups)
+    /// TODO: process.initgroups(user, extra_group)
+    /// TODO: process.version
+    /// TODO: process.versions
+    /// TODO: process.config
+    /// TODO: process.kill(pid, [signal])
+    /// TODO: process.pid
+    /// TODO: process.title
+    /// TODO: process.arch
+    /// TODO: process.platform
+    /// TODO: process.memoryUsage()
+    /// TODO: process.nextTick(callback)
+    /// TODO: process.maxTickDepth
+    /// TODO: process.umask([mask])
+    /// TODO: process.uptime()
+    /// TODO: process.hrtime()
+
+
 signals:
 
 public slots:
