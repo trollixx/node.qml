fcb5006cee1da6826f4719bb984d481ac88a494c
Oleg Shparber <trollixx@gmail.com>
2014-11-20 22:58:44 -0800

    [ErrnoExceptionObject] Use Heap name space (Qt 5.5 port)

--- src/nodeqml/types/errnoexception.cpp
+++ src/nodeqml/types/errnoexception.cpp
@@ -6,12 +6,13 @@ using namespace NodeQml;
 
 DEFINE_OBJECT_VTABLE(ErrnoExceptionObject);
 
-ErrnoExceptionObject::Data::Data(QV4::ExecutionEngine *v4, const QString &message, int errorNo,
-                                 const QString &syscall, const QString &path) :
-    QV4::ErrorObject::Data(EnginePrivate::get(v4)->errnoExceptionClass, message)
+Heap::ErrnoExceptionObject::ErrnoExceptionObject(QV4::ExecutionEngine *v4, const QString &message,
+                                                 int errorNo, const QString &syscall,
+                                                 const QString &path) :
+    QV4::Heap::ErrorObject(EnginePrivate::get(v4)->errnoExceptionClass, message)
 {
     QV4::Scope scope(v4);
-    QV4::Scoped<ErrnoExceptionObject> self(scope, this);
+    QV4::ScopedObject self(scope, this);
     QV4::ScopedValue v(scope);
 
     self->defineDefaultProperty(QStringLiteral("errno"), (v = QV4::Encode(errorNo)));
--- src/nodeqml/types/errnoexception.h
+++ src/nodeqml/types/errnoexception.h
@@ -1,16 +1,23 @@
 #ifndef ERRNOEXCEPTION_H
 #define ERRNOEXCEPTION_H
 
+#include "../v4integration.h"
+
 #include <private/qv4errorobject_p.h>
 
 namespace NodeQml {
 
+namespace Heap {
+
+struct ErrnoExceptionObject : QV4::Heap::ErrorObject {
+    ErrnoExceptionObject(QV4::ExecutionEngine *v4, const QString &message, int errorNo,
+                         const QString &syscall = QString(), const QString &path = QString());
+};
+
+} // namespace Heap
+
 struct ErrnoExceptionObject: QV4::ErrorObject {
-    struct Data : QV4::ErrorObject::Data {
-        Data(QV4::ExecutionEngine *v4, const QString &message, int errorNo,
-             const QString &syscall = QString(), const QString &path = QString());
-    };
-    V4_OBJECT(ErrorObject)
+    NODE_V4_OBJECT(ErrnoExceptionObject, ErrorObject)
 };
 
 } // namespace NodeQml
