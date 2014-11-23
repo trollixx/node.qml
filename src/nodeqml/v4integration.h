6762aed2615d17ec41356b4c50cbe07a7372ffa5
Oleg Shparber <trollixx@gmail.com>
2014-11-22 17:46:15 -0800

    [v4integration] Introduce NODE_CTX_* macros
    The aim is to minimise amount of duplicated code. It's not a final
    version, and macros can be changed or even removed.

--- src/nodeqml/v4integration.h
+++ src/nodeqml/v4integration.h
@@ -12,4 +12,11 @@
         const NodeQml::Heap::DataClass *d() const { return &static_cast<const NodeQml::Heap::DataClass &>(Managed::data); } \
         NodeQml::Heap::DataClass *d() { return &static_cast<NodeQml::Heap::DataClass &>(Managed::data); }
 
+#define NODE_CTX_CALLDATA(Context) const QV4::CallData * const callData = Context->d()->callData;
+#define NODE_CTX_V4(Context) QV4::ExecutionEngine * const v4 = Context->engine();
+
+#define NODE_CTX_SELF(Class, Context) \
+    QV4::Scope scope(Context->engine()); \
+    QV4::Scoped<Class> self(scope, Context->d()->callData->thisObject);
+
 #endif // V4INTEGRATION_H
